c3358e41d724e8f267063c10f6d6432c57a36537
kdk212 <87680596+kdk212@users.noreply.github.com>
2026-06-02 21:36:37 +0900

    Export runtime secret helpers

--- runtime/__init__.py
+++ runtime/__init__.py
@@ -1,11 +1,15 @@
 """Runtime safety helpers for agents_invest."""
 
+from .secrets import SecretLoadResult, load_runtime_secrets, public_secret_state
 from .settings import RuntimeSettings, load_runtime_settings
 from .safety import SafetyCheck, evaluate_startup_safety
 
 __all__ = [
     "RuntimeSettings",
     "SafetyCheck",
+    "SecretLoadResult",
     "evaluate_startup_safety",
+    "load_runtime_secrets",
     "load_runtime_settings",
+    "public_secret_state",
 ]
